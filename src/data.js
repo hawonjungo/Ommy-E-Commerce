fa5bf98ebce2276c8777b63b25b84273666e26b0
hawonjungo <hawon.jungo@gmail.com>
2022-07-13 01:03:56 +1000

    Create Pure React Slider

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://www.pngkey.com/png/full/792-7920081_picture-free-stock-model-background-peoplepng-com-png.png",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://www.pngkey.com/png/full/792-7920081_picture-free-stock-model-background-peoplepng-com-png.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://www.pngkey.com/png/full/792-7920081_picture-free-stock-model-background-peoplepng-com-png.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
