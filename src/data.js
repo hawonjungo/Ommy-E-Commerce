4327059cec84b76023c87ab403c8cef6cd43af36
hawonjungo <hawon.jungo@gmail.com>
2022-07-20 23:12:24 +1000

    Change Image link

--- src/data.js
+++ src/data.js
@@ -1,75 +1,75 @@
 export const sliderItems = [
-    {
-      id: 1,
-      img: "https://www.pngkey.com/png/full/792-7920081_picture-free-stock-model-background-peoplepng-com-png.png",
-      title: "SUMMER SALE",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "f5fafd",
-    },
-    {
-      id: 2,
-      img: "https://www.pngkey.com/png/full/792-7920081_picture-free-stock-model-background-peoplepng-com-png.png",
-      title: "AUTUMN COLLECTION",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "fcf1ed",
-    },
-    {
-      id: 3,
-      img: "https://www.pngkey.com/png/full/792-7920081_picture-free-stock-model-background-peoplepng-com-png.png",
-      title: "LOUNGEWEAR LOVE",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "fbf0f4",
-    },
-  ];
+  {
+    id: 1,
+    img: "https://cdn.shopify.com/s/files/1/0563/5981/3285/products/fd08f737-c270-45e6-a74a-a83d22d0b598.jpg?v=1636247543",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://cdn.shopify.com/s/files/1/0563/5981/3285/products/laurbackcrossed_720x.jpg?v=1633156870",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://cdn.shopify.com/s/files/1/0563/5981/3285/products/gusback_720x.jpg?v=1631671385",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
 
-  export const categories = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      title: "SHIRT STYLE!",
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      title: "LOUNGEWEAR LOVE",
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      title: "LIGHT JACKETS",
-    },
-  ];
-  export const popularProducts = [
-    {
-      id:1,
-      img:"",
-    },
-    {
-      id:2,
-      img:"",
-    },
-    {
-      id:3,
-      img:"",
-    },
-    {
-      id:4,
-      img:"",
-    },
-    {
-      id:5,
-      img:"",
-    },
-    {
-      id:6,
-      img:"",
-    },
-    {
-      id:7,
-      img:"",
-    },
-    {
-      id:8,
-      img:"",
-    },
-  ]
+export const categories = [
+  {
+    id: 1,
+    img: "https://cdn.shopify.com/s/files/1/0563/5981/3285/products/smalltri.jpg?v=1633187041",
+    title: "Slide Triangles",
+  },
+  {
+    id: 2,
+    img: "https://cdn.shopify.com/s/files/1/0563/5981/3285/products/smalllooptop.jpg?v=1633191309",
+    title: "The Trendiest Top",
+  },
+  {
+    id: 3,
+    img: "https://cdn.shopify.com/s/files/1/0563/5981/3285/products/waveclose.jpg?v=1636247506",
+    title: "Super G String",
+  },
+];
+export const popularProducts = [
+  {
+    id:1,
+    img:"https://cdn.shopify.com/s/files/1/0563/5981/3285/products/starfishfront_51d0446b-2cd6-45b5-8f5a-e7608253af3a.jpg?v=1637498808",
+  },
+  {
+    id:2,
+    img:"https://cdn.shopify.com/s/files/1/0563/5981/3285/products/starfishfront_e4b06dfe-af3c-4c9e-b58f-f517e91d4e7c.jpg?v=1637493638",
+  },
+  {
+    id:3,
+    img:"https://cdn.shopify.com/s/files/1/0563/5981/3285/products/0002-TOfish.jpg?v=1633186556",
+  },
+  {
+    id:4,
+    img:"https://cdn.shopify.com/s/files/1/0563/5981/3285/products/0002-TOORANGE.jpg?v=1633186556",
+  },
+  {
+    id:5,
+    img:"https://cdn.shopify.com/s/files/1/0563/5981/3285/products/FISHFRONT.jpg?v=1635084581",
+  },
+  {
+    id:6,
+    img:"https://cdn.shopify.com/s/files/1/0563/5981/3285/products/FRONTORANGE.jpg?v=1635084580",
+  },
+  {
+    id:7,
+    img:"https://cdn.shopify.com/s/files/1/0563/5981/3285/products/0001-OPwatercolorbluelimeFRONT.jpg?v=1633428265",
+  },
+  {
+    id:8,
+    img:"https://cdn.shopify.com/s/files/1/0563/5981/3285/products/0003-PAjellyfish.jpg?v=1630741489",
+  },
+]
