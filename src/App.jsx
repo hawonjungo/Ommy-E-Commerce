b9cf0ad783657d718c61f65f2dcb9f4b0a1fe406
hawonjungo <hawon.jungo@gmail.com>
2022-07-29 10:30:59 +1000

    Stripe implementation

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,22 @@ import ProductList from "./pages/ProductList";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Cart from "./pages/Cart";
+import Pay from "./components/Pay";
 
 
 const App = () => {
-  return <Login/>
+  return (
+    <Router>
+      <Switch>
+        <Route path="/pay">
+          <Pay/>
+        </Route>
+        <Route path="/success">
+          <Success/>
+        </Route>
+      </Switch>
+    </Router>
+  )
 };
 
 export default App;
