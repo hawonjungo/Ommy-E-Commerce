b2f71ccd29bb053e2e7d6503756f6814096a775f
hawonjungo <hawon.jungo@gmail.com>
2022-08-02 01:23:51 +1000

    delete pay page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import Product from "./pages/Product";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Cart from "./pages/Cart";
-import Pay from "./components/Pay";
-
 
 const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/pay">
-          <Pay/>
-        </Route>
-        <Route path="/success">
-          <Success/>
-        </Route>
-      </Switch>
-    </Router>
-  )
+  return <Cart />;
 };
 
 export default App;
