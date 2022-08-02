61e515db59db8cbd2df3f7ac4c1de9513d211ce4
hawonjungo <hawon.jungo@gmail.com>
2022-08-02 23:37:18 +1000

    change data to use product on API - error , api got blocked by cors

--- src/data.js
+++ src/data.js
@@ -27,49 +27,52 @@ export const categories = [
     id: 1,
     img: "https://cdn.shopify.com/s/files/1/0563/5981/3285/products/smalltri.jpg?v=1633187041",
     title: "Slide Triangles",
+    cat: "bottom",
   },
   {
     id: 2,
     img: "https://cdn.shopify.com/s/files/1/0563/5981/3285/products/smalllooptop.jpg?v=1633191309",
     title: "The Trendiest Top",
+    cat: "top",
   },
   {
     id: 3,
     img: "https://cdn.shopify.com/s/files/1/0563/5981/3285/products/waveclose.jpg?v=1636247506",
     title: "Super G String",
+    cat: "full",
   },
 ];
 export const popularProducts = [
   {
-    id:1,
-    img:"https://cdn.shopify.com/s/files/1/0563/5981/3285/products/starfishfront_51d0446b-2cd6-45b5-8f5a-e7608253af3a.jpg?v=1637498808",
+    id: 1,
+    img: "https://cdn.shopify.com/s/files/1/0563/5981/3285/products/starfishfront_51d0446b-2cd6-45b5-8f5a-e7608253af3a.jpg?v=1637498808",
   },
   {
-    id:2,
-    img:"https://cdn.shopify.com/s/files/1/0563/5981/3285/products/starfishfront_e4b06dfe-af3c-4c9e-b58f-f517e91d4e7c.jpg?v=1637493638",
+    id: 2,
+    img: "https://cdn.shopify.com/s/files/1/0563/5981/3285/products/starfishfront_e4b06dfe-af3c-4c9e-b58f-f517e91d4e7c.jpg?v=1637493638",
   },
   {
-    id:3,
-    img:"https://cdn.shopify.com/s/files/1/0563/5981/3285/products/0002-TOfish.jpg?v=1633186556",
+    id: 3,
+    img: "https://cdn.shopify.com/s/files/1/0563/5981/3285/products/0002-TOfish.jpg?v=1633186556",
   },
   {
-    id:4,
-    img:"https://cdn.shopify.com/s/files/1/0563/5981/3285/products/0002-TOORANGE.jpg?v=1633186556",
+    id: 4,
+    img: "https://cdn.shopify.com/s/files/1/0563/5981/3285/products/0002-TOORANGE.jpg?v=1633186556",
   },
   {
-    id:5,
-    img:"https://cdn.shopify.com/s/files/1/0563/5981/3285/products/FISHFRONT.jpg?v=1635084581",
+    id: 5,
+    img: "https://cdn.shopify.com/s/files/1/0563/5981/3285/products/FISHFRONT.jpg?v=1635084581",
   },
   {
-    id:6,
-    img:"https://cdn.shopify.com/s/files/1/0563/5981/3285/products/FRONTORANGE.jpg?v=1635084580",
+    id: 6,
+    img: "https://cdn.shopify.com/s/files/1/0563/5981/3285/products/FRONTORANGE.jpg?v=1635084580",
   },
   {
-    id:7,
-    img:"https://cdn.shopify.com/s/files/1/0563/5981/3285/products/0001-OPwatercolorbluelimeFRONT.jpg?v=1633428265",
+    id: 7,
+    img: "https://cdn.shopify.com/s/files/1/0563/5981/3285/products/0001-OPwatercolorbluelimeFRONT.jpg?v=1633428265",
   },
   {
-    id:8,
-    img:"https://cdn.shopify.com/s/files/1/0563/5981/3285/products/0003-PAjellyfish.jpg?v=1630741489",
+    id: 8,
+    img: "https://cdn.shopify.com/s/files/1/0563/5981/3285/products/0003-PAjellyfish.jpg?v=1630741489",
   },
-]
+];
